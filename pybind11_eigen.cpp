5a0907973ec67bb7bbbea39d4791f81a0c49d33f
lai <lionlai1989@gmail.com>
2022-10-03 11:29:50 +0200

    update

--- pybind11_eigen.cpp
+++ pybind11_eigen.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <pybind11/eigen.h> // It must be used here. It doesn't work with <Eigen/Dense>.
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <unsupported/Eigen/CXX11/Tensor>
 
 namespace py = pybind11;
 
@@ -12,6 +14,42 @@ Eigen::Matrix<float, 3, 3> wrapper_crossMatrix(Eigen::Matrix<float, 3, 1> v) {
   return m;
 }
 
+// return 3 dimensional ndarray with reversed order of input ndarray
+template<class T>
+py::array_t<T> eigenTensor(py::array_t<T> inArray) {
+
+    // request a buffer descriptor from Python
+    py::buffer_info buffer_info = inArray.request();
+
+    // extract data an shape of input array
+    T *data = static_cast<T *>(buffer_info.ptr);
+    std::vector<ssize_t> shape = buffer_info.shape;
+
+    // wrap ndarray in Eigen::Map:
+    // the second template argument is the rank of the tensor and has to be known at compile time
+    Eigen::TensorMap<Eigen::Tensor<T, 3>> in_tensor(data, shape[0], shape[1], shape[2]);
+
+    // build result tensor with reverse ordering
+    Eigen::Tensor<T, 3> out_tensor(2, 2, 2);
+    for (int i=0; i < shape[0]; i++) {
+        for (int j=0; j < shape[1]; j++) {
+            for (int k=0; k < shape[2]; k++) {
+                out_tensor(k, j, i) = in_tensor(i, j, k);
+            }
+        }
+
+    }
+
+    // return numpy array wrapping eigen tensor's pointer
+    return py::array_t<T>(shape, // shape
+                          {shape[0] * shape[1] * sizeof(T), shape[1] * sizeof(T), sizeof(T)},  // strides
+                          out_tensor.data());  // data pointer
+}
+
+
+
 PYBIND11_MODULE(pybind11_eigen, m) {
   m.def("crossMatrix", &wrapper_crossMatrix);
+  m.def("eigenTensor", &eigenTensor<double>, py::return_value_policy::move,
+        py::arg("inArray"));
 }
--- test_pybind11.py
+++ test_pybind11.py
@@ -4,7 +4,9 @@ import numpy as np
 from PIL import Image
 
 if __name__ == "__main__":
-    
+    a = np.array([[[1,2,3], [4,5,6]],[[7,8,9], [0,1,2]]])
+    print(a, a.shape)
+    print(pybind11_eigen.eigenTensor(a))
 
     ret = pybind11_eigen.crossMatrix(np.array([1,2,3]))
     print(ret)
